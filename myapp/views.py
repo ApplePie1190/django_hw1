9fc68e76414d1f6e9da9001dd843b55fd01c3db8
ApplePie <novikov1089@gmail.com>
2024-04-18 16:14:28 +0500

    init commit

---
+++ myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    html = """
+    <h1>Hello world</h1>
+    <p>Welcome to my site</p>
+"""
+    return HttpResponse(html)
+
+
+def about(request):
+    html = """
+    <h1>About me</h1>
+    <p>This page about me</p>
+"""
+    return HttpResponse(html)
+
+
